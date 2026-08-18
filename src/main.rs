45cbd124efa3a9b6efb99d4cd706d9fb7e8f42a5
Phan Nguyen Quoc Minh <minhpnq1807@gmail.com>
2026-08-18 17:40:02 +0700

    feat: register semantic context adapter

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,7 @@ mod scip_enrichment;
 mod scip_enrichment_integration_tests;
 mod scip_http;
 mod semantic;
+mod semantic_context;
 mod semantic_http;
 #[cfg(test)]
 mod semantic_integration_tests;
