934875f7b12e0a38d070d0e8b6ccd28cce295611
Phan Nguyen Quoc Minh <minhpnq1807@gmail.com>
2026-08-19 19:43:43 +0700

    style: load onboarding styles

--- desktop/src/renderer/main.tsx
+++ desktop/src/renderer/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
 import "./styles.css";
+import "./onboarding.css";
 
 const root = document.getElementById("root");
 if (!root) {
