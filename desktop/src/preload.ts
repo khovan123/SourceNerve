6d9c6a5942f920870b659ca6f1745d582136b191
Phan Nguyen Quoc Minh <minhpnq1807@gmail.com>
2026-08-20 09:08:30 +0700

    feat: integrate plugin verification preload API

--- desktop/src/preload.ts
+++ desktop/src/preload.ts
@@ -66,6 +66,16 @@ import {
   type IntelligenceTraceResult,
 } from "./shared/intelligence-api";
 import {
+  PLUGIN_VERIFICATION_IPC,
+  type PluginCopyResult,
+  type PluginDomainChallengeInput,
+  type PluginDomainChallengeResult,
+  type PluginIconExportResult,
+  type PluginOpenResult,
+  type PluginVerificationRunResult,
+  type PluginVerificationView,
+} from "./shared/plugin-verification-api";
+import {
   PROVIDER_WORKFLOW_IPC,
   type ProviderDefaultSyncResult,
   type ProviderIssueCreateInput,
@@ -168,6 +178,14 @@ const api: SourceNerveDesktopApi = {
   reviewDesktopTask: (taskId: string) => ipcRenderer.invoke(TASK_IPC.review, taskId) as Promise<DesktopResult<DesktopTaskReviewResult>>,
   commitDesktopTask: (input: DesktopTaskCommitInput) => ipcRenderer.invoke(TASK_IPC.commit, input) as Promise<DesktopResult<DesktopTaskCommitResult>>,
   pushDesktopTask: (taskId: string) => ipcRenderer.invoke(TASK_IPC.push, taskId) as Promise<DesktopResult<DesktopTaskPushResult>>,
+  getPluginVerificationState: () => ipcRenderer.invoke(PLUGIN_VERIFICATION_IPC.state) as Promise<DesktopResult<PluginVerificationView>>,
+  verifyPluginConnection: () => ipcRenderer.invoke(PLUGIN_VERIFICATION_IPC.verify) as Promise<DesktopResult<PluginVerificationRunResult>>,
+  copyPluginSetupFields: () => ipcRenderer.invoke(PLUGIN_VERIFICATION_IPC.copyFields) as Promise<DesktopResult<PluginCopyResult>>,
+  openChatGptPluginSetup: () => ipcRenderer.invoke(PLUGIN_VERIFICATION_IPC.openChatGpt) as Promise<DesktopResult<PluginOpenResult>>,
+  exportPluginIcon: () => ipcRenderer.invoke(PLUGIN_VERIFICATION_IPC.exportIcon) as Promise<DesktopResult<PluginIconExportResult>>,
+  setPluginDomainChallenge: (input: PluginDomainChallengeInput) => ipcRenderer.invoke(PLUGIN_VERIFICATION_IPC.challengeSet, input) as Promise<DesktopResult<PluginDomainChallengeResult>>,
+  verifyPluginDomainChallenge: () => ipcRenderer.invoke(PLUGIN_VERIFICATION_IPC.challengeVerify) as Promise<DesktopResult<PluginDomainChallengeResult>>,
+  removePluginDomainChallenge: () => ipcRenderer.invoke(PLUGIN_VERIFICATION_IPC.challengeRemove) as Promise<DesktopResult<PluginDomainChallengeResult>>,
   getProviderWorkflowState: (taskId: string) => ipcRenderer.invoke(PROVIDER_WORKFLOW_IPC.state, taskId) as Promise<DesktopResult<ProviderWorkflowState>>,
   createProviderIssue: (input: ProviderIssueCreateInput) => ipcRenderer.invoke(PROVIDER_WORKFLOW_IPC.issueCreate, input) as Promise<DesktopResult<ProviderIssueCreateResult>>,
   createProviderPull: (input: ProviderPullCreateInput) => ipcRenderer.invoke(PROVIDER_WORKFLOW_IPC.pullCreate, input) as Promise<DesktopResult<ProviderPullCreateResult>>,
