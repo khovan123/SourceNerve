5cab5e413922896a59377d0d505eb4f60ed5bf64
Phan Nguyen Quoc Minh <minhpnq1807@gmail.com>
2026-08-27 09:24:43 +0700

    style(sandbox): satisfy rustfmt import order

--- src/sandbox.rs
+++ src/sandbox.rs
@@ -1,8 +1,8 @@
-use std::path::{Path, PathBuf};
 #[cfg(target_os = "linux")]
 use std::env;
 #[cfg(target_os = "macos")]
 use std::ffi::OsString;
+use std::path::{Path, PathBuf};
 
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
